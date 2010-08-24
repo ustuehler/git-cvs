80b165bbd4f4e7506cf9041cf14f32a0b36b8563
Uwe Stuehler <uwe@bsdx.de>
2010-08-25 01:24:47 +0200

    Remove "nice" progress meter; it is too early for that

--- cvsgit/command/clone.py
+++ cvsgit/command/clone.py
@@ -8,40 +8,6 @@ from cvsgit.cvs import CVSROOT
 from cvsgit.db import Db
 from cvsgit.git import Git
 
-class StatusLine(object):
-    def __init__(self, width=72):
-        self.width = width
-        self.line = ''
-
-    def abbrev(self, line):
-        # Use up one less character column than available because some
-        # terminals will wrap to the next line if ther cursor advances
-        # past the end of the line.
-        if len(line) < self.width:
-            return line
-        else:
-            return line[:self.width-1]
-
-    def begin(self, line=''):
-        print '\r' + self.abbrev(line),
-        sys.stdout.flush()
-        self.line = line
-
-    def erase(self):
-        print '\r' + (' ' * len(self.line)),
-        sys.stdout.flush()
-        self.line = ''
-
-    def update(self, line=None):
-        if line is None: line = self.line
-        self.erase()
-        self.begin(line)
-
-    def finish(self, line=None):
-        if not line is None:
-            self.update(line)
-        print ''
-
 class clone(Cmd):
     """Clone a CVS repository into a Git repository.
 
@@ -70,35 +36,18 @@ class clone(Cmd):
             self.usage_error('too many arguments')
 
     def run(self):
-        status = StatusLine()
         git = Git(self.directory)
         git.init()
         try:
             db = Db(os.path.join(git.git_dir, 'cvsgit.db'))
             cvs = CVSROOT(self.repository)
 
-            status.nfiles = 0
-            status.nrevisions = 0
-
-            def progress(status):
-                status.nfiles += 1
-                status.update('Indexing CVS repository... ' \
-                              '(%d files, %d revisions)' % \
-                              (status.nfiles, status.nrevisions))
-
-            status.begin('Indexing CVS repository...')
-            for revision in cvs.revisions(onrcsfile=lambda ignore:
-                                              progress(status)):
-                status.nrevisions += 1
+            for revision in cvs.revisions():
                 db.add_revision(revision)
-            status.finish('Indexing CVS repository...done')
 
-            status.begin('Generating changesets...')
             for changeset in db.changesets():
                 git.add_changeset(changeset)
-            status.finish('Indexing CVS repository...done')
         except:
-            status.finish()
             git.destroy()
             raise
 
