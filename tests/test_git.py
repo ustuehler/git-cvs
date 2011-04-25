f89631b4a38ee70c770b51af6259a1cee414c9e1
Uwe Stuehler <uwe@bsdx.de>
2011-04-25 20:38:39 +0000

    Add tests/test_git.py

---
+++ tests/test_git.py
@@ -0,0 +1,17 @@
+import unittest
+from cvsgit.git import Git
+from cvsgit.rcs import RCSFile
+from os.path import dirname, join
+
+class Test(unittest.TestCase):
+
+    def testNoRevisionsOnTrunk(self):
+        # This RCS file contains no revisions on the "trunk", i.e. the
+        # first trunk revision 1.1 is explicitly marked 'dead' but it
+        # is still a branchpoint for OpenBSD release branches in which
+        # the path exists.
+        f = RCSFile(join(dirname(__file__), 'data', 'patch-copyin_c,v'))
+        for c in f.changes(): self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
