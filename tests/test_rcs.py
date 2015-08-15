44e693c2594898881f8db90a8a810d812755f932
Uwe Stuehler <uwe@bsdx.de>
2015-08-16 00:08:48 +0200

    Describe test case

--- tests/test_rcs.py
+++ tests/test_rcs.py
@@ -8,10 +8,12 @@ from cvsgit.cvs import CVS # XXX: should not be needed here
 class Test(unittest.TestCase):
 
     def testNoRevisionsOnTrunk(self):
-        # This RCS file contains no revisions on the "trunk", i.e. the
-        # first trunk revision 1.1 is explicitly marked 'dead' but it
-        # is still a branchpoint for OpenBSD release branches in which
-        # the path exists.
+        """RCS file without revisions on trunk yields no changes.
+
+        This RCS file contains no revisions on the "trunk", i.e. the first
+        trunk revision 1.1 is explicitly marked 'dead' but it is still a
+        branchpoint for OpenBSD release branches in which the path exists.
+        """
         f = RCSFile(join(dirname(__file__), 'data', 'patch-copyin_c,v'))
         for c in f.changes(): self.assertTrue(False)
 
